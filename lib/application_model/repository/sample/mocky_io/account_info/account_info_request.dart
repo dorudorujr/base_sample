907e99830f3d7c3cdee599af46234fadc9b1328f
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-08-02 21:11:48 +0900

    とりあえずparameterとrequestを作成

---
+++ lib/application_model/repository/sample/mocky_io/account_info/account_info_request.dart
@@ -0,0 +1,10 @@
+import 'package:base_sample/application_model/repository/http_request/requestable.dart';
+import 'package:base_sample/application_model/repository/sample/mocky_io/account_info/account_info_response.dart';
+import 'package:base_sample/application_model/repository/sample/util/nothing_parameter.dart';
+
+class AccountInfoRequest extends GetRequestable<AccountInfoResponse, NothingParameter> {
+  /// 通信先情報
+  String hostName = 'https://run.mocky.io';
+  String path = '/v3/fb72a27c-4fdd-49c8-8fd6-8b4ad92ac12a';
+}
+
