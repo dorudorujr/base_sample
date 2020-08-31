646b1360990aa3c7bc5d6d71c47dd976a259e8c1
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-08-31 22:01:00 +0900

    NavigationBarButton追加

---
+++ lib/widgets/navigation_bar_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class NavigationBarButton extends StatelessWidget {
+  /// Initializer Lists: コロンに続けてフィールドの初期化処理を記述
+  NavigationBarButton({
+    Key key,
+    @required this.onPressed,
+    @required String text,
+  })  : child = Text(text),
+        super(key: key);
+
+  final Widget child;
+  /// VoidCallback: 引数がなく、データを返さない
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    /// CupertinoButton: ios固有のbutton
+    return CupertinoButton(
+      child: child,
+      onPressed: onPressed,
+      padding: EdgeInsets.zero,
+    );
+  }
+}
