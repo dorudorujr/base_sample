3a4bcf138d0445fa380a53acc1d61d758f80ac65
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-09-13 20:59:07 +0900

    add cart_button.dart

---
+++ lib/pages/items_page/cart_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:base_sample/model/model.dart';
+import 'package:base_sample/pages/cart_page/cart_page.dart';
+import 'package:base_sample/widgets/widgets.dart';
+
+class CartButton extends HookWidget {
+  const CartButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final isEmpty = useProvider(
+      cartProvider.state.select((s) => s.summary.totalPrice == 0),
+    );
+    return NavigationBarButton(
+      text: useProvider(cartProvider.state.select((s) => s.summary.state)),
+      onPressed: isEmpty
+        ? null
+        : () => Navigator.of(context).push<void>(
+        CupertinoPageRoute<void>(
+          builder: (context) => const CartPage(),
+          fullscreenDialog: true,
+        ),
+      ),
+    );
+  }
+}
