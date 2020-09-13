690daeec0ffe7a5806658fe8598ccaddcb7754ca
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-09-13 21:19:57 +0900

    mainファイルを修正

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:base_sample/providers/providers.dart';
+
+import 'pages/items_page/items_page.dart';
+
+class App extends HookWidget {
+  const App({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: useProvider(navigatorKeyProvider),
+      home: const ItemsPage(),
+    );
+  }
+}
