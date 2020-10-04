0672524c9c469c89d58fece2a143e2b06690c66a
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-10-04 18:54:32 +0900

    add http_request.dart

---
+++ lib/application_model/repository/http_request/http_request.dart
@@ -0,0 +1,5 @@
+export 'package:base_sample/application_model/repository/http_request/from_map.dart';
+export 'package:base_sample/application_model/repository/http_request/parametalizable.dart';
+export 'package:base_sample/application_model/repository/http_request/request_base.dart';
+export 'package:base_sample/application_model/repository/http_request/requestable.dart';
+export 'package:base_sample/application_model/repository/http_request/responsible.dart';
