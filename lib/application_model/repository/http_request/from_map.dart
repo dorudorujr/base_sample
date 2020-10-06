5681e13a7736972ce01a7d76bb3db7d6aa427e74
杉岡成哉 <sugiokaseiya@team-lab.com>
2020-10-06 20:47:03 +0900

    add items_stock_response.dart

--- lib/application_model/repository/http_request/from_map.dart
+++ lib/application_model/repository/http_request/from_map.dart
@@ -1,5 +1,5 @@
 import 'package:base_sample/application_model/repository/sample/mocky_io/account_info/account_info_response.dart';
-
+import 'package:base_sample/application_model/repository/sample/items_stock/items_stock_response.dart';
 
 /**
  * objectのデコード用map
@@ -9,4 +9,7 @@ final Map<Type, Function> factory = <Type, Function> {
   /// sample
   AccountInfoResponse: (Map<String, dynamic> json) =>
                           AccountInfoResponse.fromJson(json),
+  /// アーキテクチャを使用したsample
+  ItemsStockResponse: (Map<String, dynamic> json) =>
+                          ItemsStockResponse.fromJson(json),
 };
