c0f2fc198716cc0ab98c2b182d01dd09d6a05e15
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-09 12:29:40 +0900

    PathParameterizableのqueryParametersをrequired指定

--- lib/application_model/repository/http_request/parametalizable.dart
+++ lib/application_model/repository/http_request/parametalizable.dart
@@ -1,8 +1,12 @@
+import 'package:base_sample/model/entities/entities.dart';
+
 abstract class Parameterizable {
   Map<String, dynamic> toJson();
 }
 
 /// Query Parameters用
 abstract class PathParameterizable extends Parameterizable {
+  PathParameterizable({required this.queryParameters});
+
   Map<String, String> queryParameters;
 }
